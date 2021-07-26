0a5deab729e27a460c52fdf49f891a780b2d9205
Sajj96 <captainsajjad4@gmail.com>
2021-07-26 15:14:10 +0300

    Added attendance controller

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+     /**
+     * Show the attendance list.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('pages.attendance.attendance');
+    }
+}
