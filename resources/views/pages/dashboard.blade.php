80fb1ab22b2a9707b234485857fbd0e0f2f4b8a7
Sajj96 <captainsajjad4@gmail.com>
2021-07-28 11:51:14 +0300

    Added bar chart at dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -49,4 +49,17 @@ Dashboard
     </div>
 </div>
 <!-- 4-blocks row end -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-header-text">Bar chart</h5>
+            </div>
+            <div class="card-block">
+                <div id="barchart" style="min-width: 250px; height: 330px; margin: 0 auto"></div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
